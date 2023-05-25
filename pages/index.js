de6891e423e0e68e7e3e3a6200f9b93f0e5e39fb
Nur Rahmat Dwi Riyanto <nrahmatd@gmail.com>
2023-05-25 17:58:29 +0700

    Update Name

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'am Nur Rahmat. I'm Software Engineer and a Gamers.</p>
+        <p>Hello, I'am Nur Rahmat D.R. I'm Software Engineer and a Gamers.</p>
         <p>
           (This is a sample website - you'll be building a site like this on{' '}
           <a href='https://nextjs.org/learn'>our Next.js tutorial</a>.
